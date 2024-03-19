332ab9fa45bec075db55bd9e6da6184a2be52b74
Synzenn <velriel42@gmail.com>
2024-03-19 11:03:04 +0700

    Membuat do while, dan menyeleseaikannya

--- PertemuanKetigaPart3/PertemuanKetigaPart3.cpp
+++ PertemuanKetigaPart3/PertemuanKetigaPart3.cpp
@@ -8,4 +8,13 @@ int main() {
 
 	x = rand() % 10;
 
-	
+	cout << "Nilai awal :" << x << endl;
+
+	do
+	{
+		cout << "Perulangan Do..While" << endl;
+		x = rand() % 10;
+
+		cout << "Nilai X : " << x << endl;
+	} while (x > 4);
+}
